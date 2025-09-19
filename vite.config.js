5237291def660bb7c6aceb516f0a2ac393082665
liuyoumi <liuyoumi@hotmail.com>
2025-09-19 15:22:04 +0800

    fix: update the vite build field base.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./bookstore",
+  base: "./",
   plugins: [
     react(),
     tailwindcss()
